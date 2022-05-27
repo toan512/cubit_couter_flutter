d5c418249fc36cb7fc3a2cb9279d8ea4910d106e
Nguyen Tien Toan <toan512@users.noreply.github.com>
2022-05-28 04:10:18 +0700

    Update couter_state.dart

--- lib/cubit/couter_state.dart
+++ lib/cubit/couter_state.dart
@@ -3,12 +3,4 @@ part of 'couter_cubit.dart';
 class CouterState {
   int counterNumber;
   CouterState({required this.counterNumber});
-
-  CouterState changeCouter({
-    required int counterNumber,
-  }){
-    return CouterState(
-      counterNumber: counterNumber,
-    );
-  }
 }
